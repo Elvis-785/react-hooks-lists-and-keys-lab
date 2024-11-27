760759296e59d4448c0dbdbdd92d144626952691
Elvis-785 <kiruielvis3358@gmail.com>
2024-11-28 00:54:02 +0300

    working with arrays and object in react

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,13 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+    // Create the linkElements variable
+   const linkElements = links.map((link) => ( <a key={link} href={`#${link}`}> {link} </a> ));
+  
+  return <nav>
+            {/* display an <a> tag for each link here */}
+            {linkElements}
+        </nav>;
 }
 
 export default NavBar;
